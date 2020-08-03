cbdc4b9bd2e44f97678520651b6d8c020a596cc0
akthrms <akt.hrms@gmail.com>
2020-08-03 18:02:02 +0900

    first commit

---
+++ src/p_11_20.clj
@@ -0,0 +1,85 @@
+(ns p-11-20)
+
+; #11
+; Maps: conj
+; Difficulty: Elementary
+; Topics:
+
+(= {:a 1, :b 2, :c 3} (conj {:a 1} [:b 2] [:c 3]))
+
+; #12
+; Intro to SequencesSolutions
+; Difficulty: Elementary
+; Topics:
+
+(= 3 (first '(3 2 1)))
+(= 3 (second [2 3 4]))
+(= 3 (last (list 1 2 3)))
+
+; #13
+; Sequences: restSolutions
+; Difficulty: Elementary
+; Topics:
+
+(= [20 30 40] (rest [10 20 30 40]))
+
+; #14
+; Intro to Functions
+; Difficulty: Elementary
+; Topics:
+
+(= 8 ((fn add-five [x] (+ x 5)) 3))
+(= 8 ((fn [x] (+ x 5)) 3))
+(= 8 (#(+ % 5) 3))
+(= 8 ((partial + 5) 3))
+
+; #15
+; Double DownSolutions
+; Difficulty: Elementary
+; Topics:
+
+(= (* 2 2) 4)
+(= (* 2 3) 6)
+(= (* 2 11) 22)
+(= (* 2 7) 14)
+
+; #16
+; Hello WorldSolutions
+; Difficulty: Elementary
+; Topics:
+
+(= (#(str "Hello, " % "!") "Dave") "Hello, Dave!")
+(= (#(str "Hello, " % "!") "Jenn") "Hello, Jenn!")
+(= (#(str "Hello, " % "!") "Rhea") "Hello, Rhea!")
+
+; #17
+; Sequences: mapSolutions
+; Difficulty: Elementary
+; Topics:
+
+(= '(6 7 8) (map #(+ % 5) '(1 2 3)))
+
+; #18
+; Sequences: filterSolutions
+; Difficulty: Elementary
+; Topics:
+
+(= '(6 7) (filter #(> % 5) '(3 4 5 6 7)))
+
+; #19
+; Last ElementSolutions
+; Difficulty: Easy
+; Topics: seqs core-functions
+
+(= (#(-> % reverse first) [1 2 3 4 5]) 5)
+(= (#(-> % reverse first) '(5 4 3)) 3)
+(= (#(-> % reverse first) ["b" "c" "d"]) "d")
+
+; #20
+; Penultimate ElementSolutions
+; Difficulty: Easy
+; Topics: seqs
+
+(= (#(nth (reverse %) 1) (list 1 2 3 4 5)) 4)
+(= (#(nth (reverse %) 1) ["a" "b" "c"]) "b")
+(= (#(nth (reverse %) 1) [[1 2] [3 4]]) [1 2])
---
+++ src/p_1_10.clj
@@ -0,0 +1,75 @@
+(ns p-1-10)
+
+; #1
+; Nothing but the Truth
+; Difficulty: Elementary
+; Topics:
+
+(= true true)
+
+; #2
+; Simple Math
+; Difficulty: Elementary
+; Topics:
+
+(= (- 10 (* 2 3)) 4)
+
+; #3
+; Intro to Strings
+; Difficulty: Elementary
+; Topics:
+
+(= "HELLO WORLD" (.toUpperCase "hello world"))
+
+; #4
+; Intro to Lists
+; Difficulty: Elementary
+; Topics:
+
+(= (list :a :b :c) '(:a :b :c))
+
+; #5
+; Lists: conj
+; Difficulty: Elementary
+; Topics:
+
+(= '(1 2 3 4) (conj '(2 3 4) 1))
+(= '(1 2 3 4) (conj '(3 4) 2 1))
+
+; #6
+; Intro to Vectors
+; Difficulty: Elementary
+; Topics:
+
+(= [:a :b :c] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))
+
+; #7
+; Vectors: conj
+; Difficulty: Elementary
+; Topics:
+
+(= [1 2 3 4] (conj [1 2 3] 4))
+(= [1 2 3 4] (conj [1 2] 3 4))
+
+; #8
+; Intro to Sets
+; Difficulty: Elementary
+; Topics:
+
+(= #{:a :b :c :d} (set '(:a :a :b :c :c :c :c :d :d)))
+(= #{:a :b :c :d} (clojure.set/union #{:a :b :c} #{:b :c :d}))
+
+; #9
+; Sets: conj
+; Difficulty: Elementary
+; Topics:
+
+(= #{1 2 3 4} (conj #{1 4 3} 2))
+
+; #10
+; Intro to Maps
+; Difficulty: Elementary
+; Topics:
+
+(= 20 ((hash-map :a 10, :b 20, :c 30) :b))
+(= 20 (:b {:a 10, :b 20, :c 30}))

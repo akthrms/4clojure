77370b4f7ad64b899748302caad213a60e6c8440
akthrms <akt.hrms@gmail.com>
2020-08-04 16:51:16 +0900

    update

--- src/p_21_30.clj
+++ src/p_21_30.clj
@@ -50,3 +50,32 @@
 (= (filter #(not (= 0 (mod % 2))) [4 2 1 6]) '(1))
 (= (filter #(not (= 0 (mod % 2))) [2 2 4 6]) '())
 (= (filter #(not (= 0 (mod % 2))) [1 1 1 3]) '(1 1 1 3))
+
+; #26
+; Fibonacci Sequence
+; Difficulty: Easy
+; Topics: Fibonacci seqs
+
+(= (#(take % (map first (iterate (fn [[a b]] [b (+ a b)]) [1 1]))) 3) '(1 1 2))
+(= (#(take % (map first (iterate (fn [[a b]] [b (+ a b)]) [1 1]))) 6) '(1 1 2 3 5 8))
+(= (#(take % (map first (iterate (fn [[a b]] [b (+ a b)]) [1 1]))) 8) '(1 1 2 3 5 8 13 21))
+
+; #27
+; Palindrome Detector
+; Difficulty: Easy
+; Topics: seqs
+
+(false? (#(= (seq %) (seq (reverse %))) '(1 2 3 4 5)))
+(true? (#(= (seq %) (seq (reverse %))) "racecar"))
+(true? (#(= (seq %) (seq (reverse %))) [:foo :bar :foo]))
+(true? (#(= (seq %) (seq (reverse %))) '(1 1 3 3 1 1)))
+(false? (#(= (seq %) (seq (reverse %))) '(:a :b :c)))
+
+; #28
+; Flatten a Sequence
+; Difficulty: Easy
+; Topics: seqs core-functions
+
+(= (#(filter (complement sequential?) (rest (tree-seq sequential? seq %))) '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))
+(= (#(filter (complement sequential?) (rest (tree-seq sequential? seq %))) ["a" ["b"] "c"]) '("a" "b" "c"))
+(= (#(filter (complement sequential?) (rest (tree-seq sequential? seq %))) '((((:a))))) '(:a))

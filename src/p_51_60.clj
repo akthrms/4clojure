1167b35f83fa5c3d12b007375d4474690d7002a3
akthrms <akt.hrms@gmail.com>
2020-10-22 17:11:28 +0900

    update

--- src/p_51_60.clj
+++ src/p_51_60.clj
@@ -13,3 +13,25 @@
 ; Topics: destructuring
 
 (= [2 4] (let [[a b c d e] [0 1 2 3 4]] [c e]))
+
+; #53
+; Longest Increasing Sub-Seq
+; Difficulty: Hard
+; Topics: seqs
+
+(defn longest-increasing-sub-seq [nums]
+  (let [sub-seq (fn [ns]
+                  (let [[h & t] ns
+                        new-ns (reduce #(if (= %2 (inc (last %1))) (conj %1 %2) (reduced %1)) [h] t)]
+                    (if (< 1 (count new-ns)) new-ns [])))]
+    (loop [new-nums nums
+           result []]
+      (if (empty? new-nums)
+        result
+        (let [new-result (sub-seq new-nums)]
+          (recur (rest new-nums) (if (< (count result) (count new-result)) new-result result)))))))
+
+(= (longest-increasing-sub-seq [1 0 1 2 3 0 4 5]) [0 1 2 3])
+(= (longest-increasing-sub-seq [5 6 1 3 2 7]) [5 6])
+(= (longest-increasing-sub-seq [2 3 3 4 5]) [3 4 5])
+(= (longest-increasing-sub-seq [7 6 5 4]) [])
